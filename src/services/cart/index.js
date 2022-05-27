1d18b8d06bdac75ecfe5212851c2fc1b2fc79700
Olayinka247 <97066655+Olayinka247@users.noreply.github.com>
2022-05-27 15:11:39 +0100

    likes included

---
+++ src/services/cart/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+import models from "../../db/models/index.js";
+
+const { Cart, Product } = models;
+
+//create router for cart
+const cartRouter = express.Router();
+
+// delete a product from cart
+cartRouter.delete("/:productId", async (req, res, next) => {
+
+export default cartRouter;
